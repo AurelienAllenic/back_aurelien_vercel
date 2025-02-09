6ec6bd77d2ddea5ba0938bb0b2d18214d50bce57
AurelienAllenic <aurelien.allenic@gmail.com>
2025-02-09 17:55:19 +0100

    (fix) trying on new vercel project

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,15 +1,19 @@
-
-const sendEmail = require('../config/mailer');
+const sendEmail = require("../config/mailer");
 
 const handleEmail = async (req, res) => {
-    const { name, email, message } = req.body;
-    try {
-        await sendEmail(email, `Contact form submission from ${name}`, message);
-        res.status(200).json({ success: true });
-    } catch (error) {
-        console.error('Erreur lors de l\'envoi de l\'email :', error);
-        res.status(500).json({ success: false });
-    }
+  const { name, email, message } = req.body;
+  console.log("Requête reçue avec :", req.body);
+
+  try {
+    console.log("Envoi de l'e-mail en cours...");
+    await sendEmail(email, `Contact form submission from ${name}`, message);
+    console.log("E-mail envoyé avec succès !");
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'e-mail :", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 module.exports = { handleEmail };

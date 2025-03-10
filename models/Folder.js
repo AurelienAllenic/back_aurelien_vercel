c1a5ce7d518eebd291214ff235b9e3894a0cef51
AurelienAllenic <aurelien.allenic@gmail.com>
2025-03-10 11:07:02 +0100

    (fix) corrected folder model

--- models/Folder.js
+++ models/Folder.js
@@ -1,13 +1,31 @@
 const mongoose = require("mongoose");
 
-const folderSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  parentFolder: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Folder",
-    default: null,
+const FolderSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    parentFolder: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Folder",
+      default: null,
+    },
+    children: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Folder",
+      },
+    ], // ✅ On garde les enfants ajoutés
+    smartLinks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SmartLinkV2",
+      },
+    ], // ✅ On remet les smartLinks
   },
-  children: [{ type: mongoose.Schema.Types.ObjectId, ref: "Folder" }],
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Folder", folderSchema);
+module.exports = mongoose.model("Folder", FolderSchema);

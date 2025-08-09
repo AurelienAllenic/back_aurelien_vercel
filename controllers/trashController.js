f580048d9d05468bb21d15b376c268c3cad96583
AurelienAllenic <aurelien.allenic@gmail.com>
2025-08-09 16:33:05 +0200

    (fix) corrected restore method for link with folder inside trash controller

--- controllers/trashController.js
+++ controllers/trashController.js
@@ -39,25 +39,59 @@ exports.restoreItem = async (req, res) => {
       return res
         .status(404)
         .json({ message: "Élément introuvable dans la corbeille" });
-
     const { entityType, data, originalId } = trashItem;
     const Model = modelsMap[entityType];
     if (!Model)
       return res
         .status(400)
         .json({ message: "Type d'entité inconnu : " + entityType });
-
     // On vérifie que l'élément n'existe pas déjà
     const exists = await Model.findById(originalId);
     if (exists)
       return res
         .status(409)
         .json({ message: `${entityType} existe déjà avec cet ID` });
-
     // On restaure l'élément supprimé
     await Model.create({ ...data, _id: originalId });
+    // Nettoyage des références invalides
+    const FolderModel = mongoose.model("Folder");
+    if (entityType === "SmartLinkV2" || entityType === "SmartLink") {
+      if (data.folder) {
+        const folderExists = await FolderModel.exists({ _id: data.folder });
+        if (!folderExists) {
+          await Model.updateOne({ _id: originalId }, { $unset: { folder: 1 } });
+        }
+      }
+    } else if (entityType === "Folder") {
+      if (data.parentFolder) {
+        const parentExists = await FolderModel.exists({
+          _id: data.parentFolder,
+        });
+        if (!parentExists) {
+          await Model.updateOne(
+            { _id: originalId },
+            { $unset: { parentFolder: 1 } }
+          );
+        }
+      }
+      // Nettoyage optionnel des enfants invalides
+      if (data.children && Array.isArray(data.children)) {
+        const validChildren = await FolderModel.find({
+          _id: { $in: data.children },
+        }).select("_id");
+        const validIds = validChildren.map((c) => c._id.toString());
+        const cleanedChildren = data.children.filter((c) =>
+          validIds.includes(c.toString())
+        );
+        if (cleanedChildren.length !== data.children.length) {
+          await Model.updateOne(
+            { _id: originalId },
+            { $set: { children: cleanedChildren } }
+          );
+        }
+      }
+    }
     await Trash.findByIdAndDelete(req.params.id);
-
     res.status(200).json({ message: `${entityType} restauré avec succès` });
   } catch (err) {
     res

2b4572a5d43c1d49122cd3850456cf23cce2f6d7
AurelienAllenic <aurelien.allenic@gmail.com>
2025-08-11 10:24:43 +0200

    (fix) corrcted cors config

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,16 +1,24 @@
 const cors = require("cors");
 
+// Middleware pour forcer la redirection vers la version sans www
+const forceNonWWW = (req, res, next) => {
+  if (req.hostname === "www.paro-musique.com") {
+    return res.redirect(301, "https://paro-musique.com" + req.originalUrl);
+  }
+  next();
+};
+
+const allowedOrigins = [
+  "https://aurelienallenic.fr",
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://paro-officiel.com",
+  /^https:\/\/(www\.)?paro-officiel\.com$/,
+  /^https:\/\/(www\.)?paro-musique\.com$/
+];
+
 const corsOptions = {
   origin: function (origin, callback) {
-    const allowedOrigins = [
-      "https://aurelienallenic.fr",
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://paro-officiel.com",
-      /^https:\/\/(www\.)?paro-officiel\.com$/,
-      /^https:\/\/(www\.)?paro-musique\.com$/
-    ];
-
     const isAllowed = !origin || allowedOrigins.some(o =>
       o instanceof RegExp ? o.test(origin) : o === origin
     );
@@ -29,16 +37,22 @@ const corsOptions = {
 const corsConfig = (req, res, next) => {
   cors(corsOptions)(req, res, () => {
     if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Origin", "*");
+      const origin = req.headers.origin;
+      if (origin && allowedOrigins.some(o =>
+        o instanceof RegExp ? o.test(origin) : o === origin
+      )) {
+        res.header("Access-Control-Allow-Origin", origin);
+      }
       res.header(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
       );
       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      res.header("Access-Control-Allow-Credentials", "true");
       return res.status(200).send();
     }
     next();
   });
 };
 
-module.exports = corsConfig;
+module.exports = { corsConfig, forceNonWWW };

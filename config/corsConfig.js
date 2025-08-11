5b6ebac3b8c5c552f11ecd49d50ef7b70195d529
AurelienAllenic <aurelien.allenic@gmail.com>
2025-08-11 10:50:35 +0200

    (fix) corrcted cors config

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -3,17 +3,16 @@ const cors = require("cors");
 const allowedPatterns = [
   /^https:\/\/(www\.)?aurelienallenic\.fr$/,
   /^https:\/\/(www\.)?paro-officiel\.com$/,
-  /^https:\/\/(www\.)?paro-musique\.com$/
+  /^https:\/\/(www\.)?paro-musique\.com$/,
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
     console.log("Origin reçu :", origin);
     if (!origin) {
-      // Pas d'origine, autoriser (ex: backend calls)
       callback(null, true);
     } else if (allowedPatterns.some((pattern) => pattern.test(origin))) {
-      callback(null, origin);
+      callback(null, true); // autorise l'origine
     } else {
       callback(new Error("CORS policy: Origin not allowed"));
     }
@@ -23,34 +22,6 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-const corsConfig = (req, res, next) => {
-  cors(corsOptions)(req, res, (err) => {
-    if (err) {
-      // Si erreur CORS, envoyer 403
-      res.status(403).send(err.message);
-      return;
-    }
-
-    // Pour toutes les requêtes, set Access-Control-Allow-Origin avec l'origine validée
-    const origin = req.headers.origin;
-    if (origin && allowedPatterns.some((pattern) => pattern.test(origin))) {
-      console.log("Origin reçu dans req.headers.origin:", req.headers.origin);
-      res.header("Access-Control-Allow-Origin", origin);
-    }
-
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-
-    next();
-  });
-};
+const corsConfig = cors(corsOptions);
 
 module.exports = corsConfig;

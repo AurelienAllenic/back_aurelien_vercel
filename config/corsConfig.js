73cfbe1787b1783989f486a12fc1e3d2f302ebbb
AurelienAllenic <aurelien.allenic@gmail.com>
2025-08-11 10:26:38 +0200

    (fix) corrcted cors config

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,29 +1,16 @@
 const cors = require("cors");
 
-// Middleware pour forcer la redirection vers la version sans www
-const forceNonWWW = (req, res, next) => {
-  if (req.hostname === "www.paro-musique.com") {
-    return res.redirect(301, "https://paro-musique.com" + req.originalUrl);
-  }
-  next();
-};
-
-const allowedOrigins = [
-  "https://aurelienallenic.fr",
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "https://paro-officiel.com",
-  /^https:\/\/(www\.)?paro-officiel\.com$/,
-  /^https:\/\/(www\.)?paro-musique\.com$/
-];
-
 const corsOptions = {
   origin: function (origin, callback) {
-    const isAllowed = !origin || allowedOrigins.some(o =>
-      o instanceof RegExp ? o.test(origin) : o === origin
-    );
+    const allowedOrigins = [
+      "https://aurelienallenic.fr",
+      "https://www.aurelienallenic.fr",
+      "https://paro-officiel.com",
+      "https://paro-musique.com",
+      "https://www.paro-musique.com/",
+    ];
 
-    if (isAllowed) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("CORS policy: Origin not allowed"));
@@ -37,22 +24,16 @@ const corsOptions = {
 const corsConfig = (req, res, next) => {
   cors(corsOptions)(req, res, () => {
     if (req.method === "OPTIONS") {
-      const origin = req.headers.origin;
-      if (origin && allowedOrigins.some(o =>
-        o instanceof RegExp ? o.test(origin) : o === origin
-      )) {
-        res.header("Access-Control-Allow-Origin", origin);
-      }
+      res.header("Access-Control-Allow-Origin", "*");
       res.header(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
       );
       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      res.header("Access-Control-Allow-Credentials", "true");
       return res.status(200).send();
     }
     next();
   });
 };
 
-module.exports = { corsConfig, forceNonWWW };
+module.exports = corsConfig;

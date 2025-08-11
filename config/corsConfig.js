de3567be4ba36e4159e8e2b92791157f75f865f8
AurelienAllenic <aurelien.allenic@gmail.com>
2025-08-11 10:30:22 +0200

    (fix) corrcted cors config

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -2,15 +2,16 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: function (origin, callback) {
-    const allowedOrigins = [
-      "https://aurelienallenic.fr",
-      "https://www.aurelienallenic.fr",
-      "https://paro-officiel.com",
-      "https://paro-musique.com",
-      "https://www.paro-musique.com",
+    const allowedPatterns = [
+      /^https:\/\/(www\.)?aurelienallenic\.fr$/,
+      /^https:\/\/(www\.)?paro-officiel\.com$/,
+      /^https:\/\/(www\.)?paro-musique\.com$/
     ];
 
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (
+      !origin ||
+      allowedPatterns.some((pattern) => pattern.test(origin))
+    ) {
       callback(null, true);
     } else {
       callback(new Error("CORS policy: Origin not allowed"));

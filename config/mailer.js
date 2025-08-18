e1f1ca2b3c3bf94340542a663f539f5a0879d6ec
AurelienAllenic <aurelien.allenic@gmail.com>
2025-08-18 17:05:01 +0200

    (fix) corrected secure option

--- config/mailer.js
+++ config/mailer.js
@@ -1,21 +1,21 @@
-const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    seucre: true,
-    auth: {
-        user: process.env.SMTP_EMAIL,
-        pass : process.env.SMTP_PASSWORD,
-    },
-    port: 465,
-    host: "smtp.gmail.com",
+  service: "gmail",
+  secure: true,
+  auth: {
+    user: process.env.SMTP_EMAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+  port: 465,
+  host: "smtp.gmail.com",
 });
 
 const sendEmail = async (to, subject, text) => {
-    const mailOptions = { from: process.env.SMTP_EMAIL, to, subject, text };
-    await transporter.sendMail(mailOptions);
+  const mailOptions = { from: process.env.SMTP_EMAIL, to, subject, text };
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;

540e14b090013656b9da94e2012088b3d52942ec
AurelienAllenic <aurelien.allenic@gmail.com>
2025-10-05 16:20:34 +0200

    (feat) added debug log

--- routes/counterRoutes.js
+++ routes/counterRoutes.js
@@ -1,25 +1,35 @@
-const express = require('express');
-const { incrementCounter, getCounter } = require('../controllers/counterController');
-const auth = require('../middlewares/authMiddleware');
+const express = require("express");
+const {
+  incrementCounter,
+  getCounter,
+} = require("../controllers/counterController");
+const auth = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.get('/increment-qr', (req, res) => incrementCounter(req, res, 'qrClickCount'));
+router.get("/increment-qr", (req, res) =>
+  incrementCounter(req, res, "qrClickCount")
+);
 
-router.get('/check-qr-clicks', auth, (req, res) => getCounter(req, res, 'qrClickCount'));
-
-router.get('/increment-visits', (req, res) => incrementCounter(req, res, 'visits'));
-
-router.get('/check-visits', auth, (req, res) => getCounter(req, res, 'visits'));
-
-router.get('/increment-counter/:name', (req, res) => {
-    const { name } = req.params;
-    incrementCounter(req, res, name);
+router.get("/check-qr-clicks", auth, (req, res) => {
+  console.log("Session dans check-qr-clicks:", req.session); // DEBUG
+  getCounter(req, res, "qrClickCount");
 });
 
-router.get('/check-counter/:name', auth, (req, res) => {
-    const { name } = req.params;
-    getCounter(req, res, name);
+router.get("/increment-visits", (req, res) =>
+  incrementCounter(req, res, "visits")
+);
+
+router.get("/check-visits", auth, (req, res) => getCounter(req, res, "visits"));
+
+router.get("/increment-counter/:name", (req, res) => {
+  const { name } = req.params;
+  incrementCounter(req, res, name);
+});
+
+router.get("/check-counter/:name", auth, (req, res) => {
+  const { name } = req.params;
+  getCounter(req, res, name);
 });
 
 module.exports = router;

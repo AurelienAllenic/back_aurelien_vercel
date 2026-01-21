4f7385f434b80961dc64576b9a4d89e9a6a48a0f
AurelienAllenic <aurelien.allenic@gmail.com>
2026-01-21 19:35:46 +0100

    (fix) simpliefied cors for aurelienallenic.fr

--- routes/aurelienRoutes.js
+++ routes/aurelienRoutes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const aurelienContactController = require("../controllers/aurelienContactController");
-const corsConfig = require("../config/corsConfig");
-
-// Gérer explicitement les requêtes OPTIONS (preflight) pour CORS
-router.options("/aurelien-contact", corsConfig);
 
 // AURELIEN CONTACT ROUTE
 router.post("/aurelien-contact", aurelienContactController.handleAurelienContact);
